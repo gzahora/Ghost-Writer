e7e6f7b2d16b01e4cd91ed4151e5a78f0174be6d
Greg Zahora <gregzahora3@gmail.com>
2019-09-03 11:45:43 -0400

    got new story link working

--- client/src/pages/NewStory.js
+++ client/src/pages/NewStory.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
