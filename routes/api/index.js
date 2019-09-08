7118cffeffc91cac3e12312266d6045941d1b859
Matthew Dague <mattdague@gmail.com>
2019-09-08 13:57:01 -0400

    added usernames to navbar and updated user icons for cards and navbar

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,15 +2,12 @@ const router = require("express").Router();
 const storyRoutes = require("./stories");
 const inProgressRoutes = require("./inProgress");
 const sectionsRoutes = require("./sections");
-const usersRoutes = require("./users");
-
 
 
 // Story routes
 router.use("/stories", storyRoutes);
 router.use("/inProgress", inProgressRoutes);
 router.use("/sections", sectionsRoutes);
-router.use("/users", usersRoutes);
 
 
 module.exports = router;
