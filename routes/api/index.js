b15bd211622d659fbad709b9d3ee81836208856c
Greg Zahora <gregzahora3@gmail.com>
2019-09-08 14:12:39 -0400

    fixed merge conflict

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,6 @@ const router = require("express").Router();
 const storyRoutes = require("./stories");
 const inProgressRoutes = require("./inProgress");
 const sectionsRoutes = require("./sections");
-<<<<<<< HEAD
-
-=======
->>>>>>> c0f9e435ffaeb02c015090d8fc99e54daee2b091
 
 
 // Story routes
