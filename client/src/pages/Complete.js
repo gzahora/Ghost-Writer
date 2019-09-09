c3a1f1ee2dccff90dbc98783c264f14039f64cc1
Greg Zahora <gregzahora3@gmail.com>
2019-09-09 19:34:58 -0400

    created example of plan with the setting portion in the Complete.js page

--- client/src/pages/Complete.js
+++ client/src/pages/Complete.js
@@ -2,7 +2,15 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
+import SettingModal from "../components/InfoModal/setting";
+import FirstPlotPointModal from "../components/InfoModal/FirstPlotPoint";
+import MidpointModal from "../components/InfoModal/Midpoint";
+import ClimaxModal from "../components/InfoModal/Climax";
+import ResolutionModal from "../components/InfoModal/Resolution";
 import API from "../utils/API";
+import { Redirect } from 'react-router-dom'
+import { TextArea, FormBtn } from "../components/Form";
+import axios from "axios";
 import Nav from "../../src/components/Nav";
 
 class Complete extends Component {
@@ -25,46 +33,87 @@ class Complete extends Component {
     return (
       <div>
         <Nav />
-        <Container fluid>
+        <Container fluid className="text-center">
+          <SettingModal />
+          <FirstPlotPointModal />
+          <MidpointModal />
+          <ClimaxModal />
+          <ResolutionModal />
           <Row>
+            <Container>
             <Col size="md-12">
             <Jumbotron>
                 <h1 className="storyTitle">"{this.state.story.title}"</h1>
                 <h2>Genre: {this.state.story.genre}</h2>
               </Jumbotron>
               <article>
-                <h3>Setting: </h3>
-                <h5>by {this.state.story.user ? this.state.story.user.username : " "}</h5>
-                <button class="infoBtn" data-toggle="modal" data-target="#settingModal"><i class="fa fa-info"></i></button>
+              <div className="align-middle">
+                <button className="infoBtn" data-toggle="modal" data-target="#settingModal"><i className="fa fa-info"></i></button>
+                &nbsp;&nbsp;&nbsp;
+                <h3>Setting -  {this.state.story.user ? this.state.story.user.username : " "}</h3>
                 <p>
                   {this.state.story.setting}
                 </p>
+                </div>
+                <div className="align-middle">
+                <button className="infoBtn" data-toggle="modal" data-target="#settingModal"><i className="fa fa-info"></i></button>
+                &nbsp;&nbsp;&nbsp;
+                <h3>Plot Point -  {this.state.story.plot_point ? this.state.story.plot_point.user.username : " "}</h3>
+                <p>
+                  {this.state.story.setting}
+                </p>
+                </div>
+                <div className="align-middle">
+                <button className="infoBtn" data-toggle="modal" data-target="#settingModal"><i className="fa fa-info"></i></button>
+                &nbsp;&nbsp;&nbsp;
+                <h3>Setting -  {this.state.story.user ? this.state.story.user.username : " "}</h3>
+                <p>
+                  {this.state.story.setting}
+                </p>
+                </div>
+                <div className="align-middle">
+                <button className="infoBtn" data-toggle="modal" data-target="#settingModal"><i className="fa fa-info"></i></button>
+                &nbsp;&nbsp;&nbsp;
+                <h3>Setting -  {this.state.story.user ? this.state.story.user.username : " "}</h3>
+                <p>
+                  {this.state.story.setting}
+                </p>
+                </div>
+                <div className="align-middle">
+                <button className="infoBtn" data-toggle="modal" data-target="#settingModal"><i className="fa fa-info"></i></button>
+                &nbsp;&nbsp;&nbsp;
+                <h3>Setting -  {this.state.story.user ? this.state.story.user.username : " "}</h3>
+                <p>
+                  {this.state.story.setting}
+                </p>
+                </div>
                 <h3>Plot Point: </h3>
                 <h5>by {this.state.story.plot_point ? this.state.story.plot_point.user.username : " "}</h5>
-                <button class="infoBtn" data-toggle="modal" data-target="#plotPointModal"><i class="fa fa-info"></i></button>
+                <button className="infoBtn" data-toggle="modal" data-target="#plotPointModal"><i className="fa fa-info"></i></button>
                 <p>
                   {this.state.story.plot_point ? this.state.story.plot_point.section_text : "Section has not yet been created"}
                 </p>
                 <h3>Midpoint: </h3>
                 <h5>by {this.state.story.midpoint ? this.state.story.midpoint.user.username : " "}</h5>
-                <button class="infoBtn" data-toggle="modal" data-target="#midpointModal"><i class="fa fa-info"></i></button>
+                <button className="infoBtn" data-toggle="modal" data-target="#midpointModal"><i className="fa fa-info"></i></button>
                 <p>
                   {this.state.story.midpoint ? this.state.story.midpoint.section_text : "Section has not yet been created"}
                 </p>
                 <h3>Climax: </h3>
                 <h5>by {this.state.story.climax ? this.state.story.climax.user.username : " "}</h5>
-                <button class="infoBtn" data-toggle="modal" data-target="#climaxModal"><i class="fa fa-info"></i></button>
+                <button className="infoBtn" data-toggle="modal" data-target="#climaxModal"><i className="fa fa-info"></i></button>
                 <p>
                   {this.state.story.climax ? this.state.story.climax.section_text : "Section has not yet been created"}
                 </p>
                 <h3>Resolution: </h3>
                 <h5>by {this.state.story.resolution ? this.state.story.plot_point.user.username : " "}</h5>
-                <button class="infoBtn" data-toggle="modal" data-target="#resolutionModal"><i class="fa fa-info"></i></button>
+                <button className="infoBtn" data-toggle="modal" data-target="#resolutionModal"><i className="fa fa-info"></i></button>
                 <p>
                   {this.state.story.resolution ? this.state.story.resolution.section_text : "Section has not yet been created"}
                 </p>
               </article>
             </Col>
+            </Container>
           </Row>
           <footer></footer>
         </Container>
