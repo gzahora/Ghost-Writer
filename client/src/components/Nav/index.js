fd206aac39b7c1f45b5fc27d13205d4a3007906c
Ryan Lacon <ryan.lacon@gmail.com>
2019-09-15 10:47:56 -0400

    Removed commented out lines of code

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -48,7 +48,6 @@ class Nav extends Component {
   }
 
   render() {
-    {/* <nav className="navbar navbar-expand-lg navbar-dark bg-info collapse navbar-collapse"> */ }
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-info">
         <Link className="navbar-brand" to="/AllInProgress">Ghost Writer</Link>
@@ -81,39 +80,6 @@ class Nav extends Component {
           </form>
         </div>
       </nav>
-      // <nav className="navbar bg-info navbar-left navbar-expand-sm">
-      //   <div className="container-fluid">
-      //     <div className="navbar-header">
-      //       <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-      //         <span className="sr-only">Toggle navigation</span>
-      //         <i className="fa fa-bars"></i>
-      //       </button>
-      //       <Link className="navbar-brand" to="/AllInProgress">Ghost Writer</Link>
-      //     </div>
-      //     <div id="navbar" className="collapse navbar-collapse">
-      //       <ul className="nav navbar-nav">
-      //         <Link className="nav-item nav-link" to="/AllInProgress">In-progress stories</Link>
-      //         <Link className="nav-item nav-link" to="/AllComplete">Complete stories</Link>
-      //       </ul>
-
-      //       <ul className="nav navbar-nav navbar-right">
-      //         {/* <div className="d-flex justify-content-end"> */}
-      //         <Link className="nav-profile" to="/Profile">{this.state.user.username}</Link>
-      //         <img className="profile-pic" src="https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/pencil-512.png" alt="User pic" />
-      //         <div className="dropdown show">
-      //           <Link to="/Profile" className="btn btn-secondary dropdown-toggle profileDropdown" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      //             My profile
-      //           </Link>
-      //           {this.renderRedirect()}
-      //           <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-      //             <Link to="/profile" className="dropdown-item">Profile</Link>
-      //             <Link to="/" className="dropdown-item" onClick={this.logout}>Sign Out</Link>
-      //           </div>
-      //         </div>
-      //       </ul>
-      //     </div>
-      //   </div>
-      // </nav>
     );
   }
 }
